356894107296ab6a73395425fb830e5532c00457
Jonathan Ivan Perez Jasso <ivan0410jpj@gmail.com>
2023-08-28 12:42:16 -0600

    Primera parte del menu

---
+++ menu.py
@@ -0,0 +1,14 @@
+
+nota = []
+folio = 1
+
+while True:
+    print("Bienvenido al menu de opciones del taller mecanico")
+    opcion = int(input("1-Registrar Nota\n2-Consultas y reportes\n3-Cancelar Nota\n4-Recuperar una nota\n5-Salir del Sistema"))
+    
+    if opcion == 1:
+        folio =+ 1
+        nombre = input("Ingresa el nombre completo del cliente")
+        break
+
+
