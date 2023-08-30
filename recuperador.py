df073b6a3b0e40d0860aa97af3521cdcebbdcef7
CantuDiazEdgarIvan <greyzamora43@gmail.com>
2023-08-29 22:32:25 -0600

    segundo avance

--- recuperador.py
+++ recuperador.py
@@ -3,7 +3,7 @@ papelera={1: ['Reparación', '08/09/2023', '27/10/2023'],
     3: ['Recolección de residuos', '03/09/2023', '10/09/2023'],
     4: ['Inspección de equipos', '12/11/2023', '18/11/2023'],
     5: ['Capacitación sobre reciclaje', '22/09/2023', '30/09/2023']}
-
+        
 dic_principal={6: ['Reparación', '08/09/2023', '27/10/2023']}
 
 def recuperador_archivos():
@@ -41,6 +41,42 @@ def recuperador_archivos():
             break
 
 
+# Cancelar una nota: Se solicitará al usuario el folio de la nota a cancelar; 
+# si la nota existe se desplegarán los datos de la nota indicada y todo el detalle que la conforma y 
+# se le solicitará al usuario la confirmación para proceder a cancelar la nota antes de realizar dicha operación 
+# considerando que puede no confirmarla y entonces no se procederá a la cancelación y se le informará que dicha 
+# nota no fue cancelada. Si el folio indicado no existe o corresponde a una nota cancelada, se debe informar que 
+#no está en el sistema. Considere que cancelar una nota no la debe eliminar realmente, simplemente se debe ignorar 
+# para cualquier reporte que no sea de notas canceladas.
 
-recuperador_archivos()
-print(dic_principal)
+dic={1: ['Reparación', '08/09/2023', '27/10/2023'],
+    2: ['Mantenimiento', '15/10/2023', '20/11/2023'],
+    3: ['Recolección de residuos', '03/09/2023', '10/09/2023'],
+    4: ['Inspección de equipos', '12/11/2023', '18/11/2023'],
+    5: ['Capacitación sobre reciclaje', '22/09/2023', '30/09/2023']}
+
+def eliminador_notas():
+    try:
+        opcion=int(input('INGRESA EL FOLIO A ELIMINAR: \n'))
+    except Exception:
+        print('el dato debe ser de tipo entero no str')
+    else:
+        if opcion in papelera:
+            recuperador=papelera[opcion]
+            print('\nFOLIO\tNOTA\t\tFECHA DE INICIO\t\tTFECHA DE ENTREGA')
+            print(f'{opcion}:\t{recuperador[0]}\t{recuperador[1]}\t\t{recuperador[2]}')
+        else:
+            print(f'EL FOLIO {opcion} NO EXISTE')
+
+    print('[1] ELIMINAR\n[2] CANCELAR')
+    opcion_2=int(input('SELECCIONA: \n'))
+    print("Debes ingresas un dato entero")
+    if (opcion_2 == 1):
+        dic.pop(opcion)
+    if (opcion_2 == 2):
+        print('NO SE HA PODIDO RECUPEAR')
+    
+        
+
+eliminador_notas()
+print(dic)
