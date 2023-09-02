cb6c9e10751ac7437291520d1da105effc0da4b0
Jonathan Ivan Perez Jasso <ivan0410jpj@gmail.com>
2023-09-02 15:17:24 -0600

    Se agrego la funcion de eliminar una nota y consultar una nota

--- menu.py
+++ menu.py
@@ -1,14 +1,16 @@
+diccionario = {
+    1: ["Reparaciond de motor"],
+    2: ["Cambio de llantas"],
+    3: ["Refaccion de asientos"]
+}
 
-nota = []
-folio = 1
+variable = int(input("Dime el id de la clave que borraras"))
+diccionario.pop(variable)
+print(f'Folio\tNotas')
+for folio , notas in diccionario.items():
+    print(f'{folio}\t{notas[0]}')
 
-while True:
-    print("Bienvenido al menu de opciones del taller mecanico")
-    opcion = int(input("1-Registrar Nota\n2-Consultas y reportes\n3-Cancelar Nota\n4-Recuperar una nota\n5-Salir del Sistema"))
-    
-    if opcion == 1:
-        folio =+ 1
-        nombre = input("Ingresa el nombre completo del cliente")
-        break
-
-
+consultar = int(input("Escribe el id de la nota que buscas"))
+for folio , notas in diccionario.items():
+    if consultar == folio:
+        print(f'{folio}\t{notas[0]}')
